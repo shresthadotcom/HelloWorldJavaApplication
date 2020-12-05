e213c73bebb552fc296ff189a6fc7128bc16862f
Nisha Shrestha <shrestha.s.nisha@gmail.com>
2020-12-05 14:01:52 -0600

    Updated Main.java by removing commented code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world ! from github.com..");
-        System.out.print("$$$SANDESH");
-        //System.out.println("Java Automation\n");
-
-        /*
-        //4/6/2020 2:20 pm
-        int[] intArray={100,200,300};
-        for (int a:intArray){
-            System.out.println(a);
-        }
-        */
-        //https://github.com/qalamsal/gitTest_HelloWorld_Project.git
+        System.out.print("$$$NISHA");
+        System.out.println("Java Automation\n");
     }
 }
